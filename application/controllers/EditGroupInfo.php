0973f51521f1eaaf45481f61147b7eea57795cbe
Hinata <869628168@qq.com>
2018-05-12 18:56:24 +0800

    update

--- application/controllers/EditGroupInfo.php
+++ application/controllers/EditGroupInfo.php
@@ -11,10 +11,13 @@ class EditGroupInfo extends CI_Controller
     public function index()
     {
         $arguments = array_merge($this->input->post(),$this->input->get());
+        //echo json_encode($arguments);exit(0);
         $this->load->model('user');
         $this->load->model('shop');
         $this->load->model('group');
-        $user_info = $this->user->get_user_info_by_phone($arguments['phone']);
+        $this->load->helper('user');
+        $help_user = new user_helper();
+        $user_info = $this->user->get_user_info_by_user_id($arguments['uid']);
         $group_info = $this->group->get_group_info_by_uid($user_info['uid']);
         if(!isset($arguments['flag'])){
             $data = array(
@@ -24,13 +27,10 @@ class EditGroupInfo extends CI_Controller
             $this->load->view('Group/editgroupinfo',$data);
             return;
         }
-
-
+        $arguments['gid'] = $group_info['gid'];
         $this->load->model('group');
         $group_info = $this->group->update_group_info($arguments);
-        $data = array(
-            'group_info' => $group_info,
-        );
+        $data = $help_user->show_group($user_info,$arguments);
         $this->load->view('Group/groupuserhome',$data);
     }
 }
